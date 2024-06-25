74fc75b140949d0defe2571e75b0379b7b916f46
Shimanshu Yadav <shimanshuyadav83@gmail.com>
2024-06-25 06:59:07 +0530

    added job description

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -6,6 +6,8 @@ function Work() {
 
         <>
 
+
+
             <div className='w-full h-full  border-l-neutral-800 border-l flex flex-row ms-4 gap-3 pb-8'>
 
                 {/* dot  style={{transform:" translateX(50%)"}}  */}
@@ -24,77 +26,181 @@ function Work() {
 
                         {/* company name */}
                         <h1 className=" text-white text-lg">
-                            Oneture Technologies 
+                            Vernost Marketing Technology PVT. LTD.
                         </h1>
 
                         <span className='my-1 mx-2 text-sm text-t-accent'  >
-                            March 2024 — Present
+                            April 2022 — March 2024
                         </span>
                     </div>
 
 
                     {/* all the work will be placed here  */}
 
-                    <ol className=' list-disc  text-neutral-500'  >
-                        <li className='text-neutral-500  text-sm sm:leading-7' >
-                            A lightweight css framework for building fast and flexible interfaces using pre-defined style classes.
+                    <ol class='list-decimal text-neutral-500'>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Developed and maintained web applications using
+                            <span class="ml-1 text-neutral-200">Node.js</span> ,
+                            <span class="ml-1 text-neutral-200">Express.js</span> ,
+                            <span class="ml-1 text-neutral-200">Angular</span> ,
+                            <span class="ml-1 text-neutral-200">Mysql</span> ,
+                            <span class="ml-1 text-neutral-200">Redis</span> ,
+                            <span class="ml-1 text-neutral-200">HTML</span> ,
+                            <span class="ml-1 text-neutral-200">CSS</span> ,
+                            <span class="ml-1 text-neutral-200">JavaScript</span> and other technologies.
                         </li>
-
-                        <li className='text-neutral-500  text-sm sm:leading-7' >
-                            A lightweight css framework for building fast and flexible interfaces using pre-defined style classes.
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Worked on crucial features of loyalty applications such as
+                            <span class="ml-1 text-neutral-200">Rule Engine</span> ,
+                            <span class="ml-1 text-neutral-200">Coupon Engine</span> ,
+                            <span class="ml-1 text-neutral-200">Fraud Detection</span> , etc.
                         </li>
-
-
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Wrote clean, modular, and well-documented code, adhering to
+                            <span class="ml-1 text-neutral-200">best practices</span> and
+                            <span class="ml-1 text-neutral-200">design patterns</span> , for effortless collaboration and future updates.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Worked on
+                            <span class="ml-1 text-neutral-200">multiple projects</span> for multiple clients.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            In one project, I was the
+                            <span class="ml-1 text-neutral-200">only developer</span> and was responsible for handling clients and providing them an efficient solution.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Created an
+                            <span class="ml-1 text-neutral-200">Angular component library</span> that is currently used in all projects.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Guided and mentored
+                            <span class="ml-1 text-neutral-200">freshers</span> in the organization.
+                        </li>
                     </ol>
+
+
 
                 </div>
             </div>
 
 
             {/* second job div */}
-        <div className='w-full h-full  border-l-neutral-800 border-l flex flex-row ms-4 gap-3'>
+            <div className='w-full h-full  border-l-neutral-800 border-l flex flex-row ms-4 gap-3'>
 
-            {/* dot  style={{transform:" translateX(50%)"}}  */}
-            <div className="-translate-x-2/4  w-3 h-3 rounded-full mt-2 gap-2  border border-neutral-800 border-background bg-neutral-800"></div>
+                {/* dot  style={{transform:" translateX(50%)"}}  */}
+                <div className="-translate-x-2/4  w-3 h-3 rounded-full mt-2 gap-2  border border-neutral-800 border-background bg-neutral-800"></div>
 
-            {/* different component will be visible to you here  */}
-            <div className="w-full flex flex-col"  >
+                {/* different component will be visible to you here  */}
+                <div className="w-full flex flex-col"  >
 
-                {/* role */}
-                <h1 className=" mb-2 text-white text-base">
-                    Vernost Marketing and technologies limited 
-                </h1>
-
-
-                <div className="flex flex-col md:justify-between md:flex-row pb-2  ">
-
-                    {/* company name */}
-                    <h1 className=" text-white text-lg">
-                        Oneture Technologies 
+                    {/* role */}
+                    <h1 className=" mb-2 text-white text-base">
+                        Intern Software Developer
                     </h1>
 
-                    <span className='my-1 mx-2 text-sm text-t-accent'  >
-                        March 2024 — Present
-                    </span>
+
+                    <div className="flex flex-col md:justify-between md:flex-row pb-2  ">
+
+                        {/* company name */}
+                        <h1 className=" text-white text-lg">
+                            Emortech Systems
+                        </h1>
+
+                        <span className='my-1 mx-2 text-sm text-t-accent'  >
+                            Jan 2022 - March 2022
+                        </span>
+                    </div>
+
+
+                    {/* all the work will be placed here  */}
+
+                    <ol class='list-decimal text-neutral-500'>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Collaborate with the development team to
+                            <span class="ml-1 text-neutral-200">design</span> ,
+                            <span class="ml-1 text-neutral-200">implement</span> , and
+                            <span class="ml-1 text-neutral-200">maintain</span>
+                            server-side code that powers the application’s core functionality.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Created
+                            <span class="ml-1 text-neutral-200">Microsoft</span> and
+                            <span class="ml-1 text-neutral-200">Slack bots</span> for various use cases.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Investigate and
+                            <span class="ml-1 text-neutral-200">resolve issues</span> reported by users or detected through testing,
+                            <span class="ml-1 text-neutral-200">debugging code</span> , and providing solutions.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Created
+                            <span class="ml-1 text-neutral-200">Express.js</span>
+                            application backend from scratch for blog websites.
+                        </li>
+                    </ol>
+
+
                 </div>
-
-
-                {/* all the work will be placed here  */}
-
-                <ol className=' list-disc  text-neutral-500'  >
-                    <li className='text-neutral-500  text-sm sm:leading-7' >
-                        A lightweight css framework for building fast and flexible interfaces using pre-defined style classes.
-                    </li>
-
-                    <li className='text-neutral-500  text-sm sm:leading-7' >
-                        A lightweight css framework for building fast and flexible interfaces using pre-defined style classes.
-                    </li>
-
-                    
-                </ol>
-
             </div>
-        </div>
+
+            {/* second job div */}
+            <div className='w-full h-full  border-l-neutral-800 border-l flex flex-row ms-4 gap-3'>
+
+                {/* dot  style={{transform:" translateX(50%)"}}  */}
+                <div className="-translate-x-2/4  w-3 h-3 rounded-full mt-2 gap-2  border border-neutral-800 border-background bg-neutral-800"></div>
+
+                {/* different component will be visible to you here  */}
+                <div className="w-full flex flex-col"  >
+
+                    {/* role */}
+                    <h1 className=" mb-2 text-white text-base">
+                        Full Stack Developer
+                    </h1>
+
+
+                    <div className="flex flex-col md:justify-between md:flex-row pb-2  ">
+
+                        {/* company name */}
+                        <h1 className=" text-white text-lg">
+                            Oneture Technologies
+                        </h1>
+
+                        <span className='my-1 mx-2 text-sm text-t-accent'  >
+                            March 2022 - Present
+                        </span>
+                    </div>
+
+
+                    {/* all the work will be placed here  */}
+
+                    <ol class='list-decimal text-neutral-500'>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Collaborate with the development team to
+                            <span class="ml-1 text-neutral-200">design</span> ,
+                            <span class="ml-1 text-neutral-200">implement</span> , and
+                            <span class="ml-1 text-neutral-200">maintain</span>
+                            server-side code that powers the application’s core functionality.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Created
+                            <span class="ml-1 text-neutral-200">Microsoft</span> and
+                            <span class="ml-1 text-neutral-200">Slack bots</span> for various use cases.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Investigate and
+                            <span class="ml-1 text-neutral-200">resolve issues</span> reported by users or detected through testing,
+                            <span class="ml-1 text-neutral-200">debugging code</span> , and providing solutions.
+                        </li>
+                        <li class='text-neutral-500 text-sm sm:leading-7'>
+                            Created
+                            <span class="ml-1 text-neutral-200">Express.js</span>
+                            application backend from scratch for blog websites.
+                        </li>
+                    </ol>
+
+
+                </div>
+            </div>
 
 
 
