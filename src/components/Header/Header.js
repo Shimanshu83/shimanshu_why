450345bee2e5ddc1bd4718e20decac49854fb5f5
Shimanshu Yadav <shimanshuyadav83@gmail.com>
2025-02-22 20:44:33 +0530

    corrected allignment issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,42 +17,44 @@ function Header() {
         A highly motivated <span class="font-bold">full-stack developer</span> with <span class="font-bold">3 years of experience</span>, known for taking ownership of projects from <span class="font-bold">inception to deployment</span>. Passionate about learning new technologies, solving complex problems, and working across diverse tech stacks. Proven track record of building <span class="font-bold">scalable applications</span>, optimizing performance, and delivering high-quality solutions across <span class="font-bold">frontend</span>, <span class="font-bold">backend</span>, and <span class="font-bold">DevOps</span>.
       </p>
 
-      <div className='flex justify-between flex-center' >
+      <div className='flex justify-between flex-center flex-col md:flex-row' >
 
         <div className='flex flex-center text-t-secondary-dark space-x-6 pt-4'>
-          <Link 
-            to='/' 
+          <Link
+            to='/'
             className={`flex flex-center space-x-2 cursor-pointer hover:text-t-accent ${location.pathname === '/' ? 'text-t-accent' : 'text-t-secondary-dark'}`}
           >
             <span>home</span>
             <FontAwesomeIcon width={12} height={12} icon={faArrowUpRightFromSquare} />
           </Link>
 
-          <Link 
-            to='/work' 
+          <Link
+            to='/work'
             className={`flex flex-center space-x-2 cursor-pointer hover:text-t-accent ${location.pathname === '/work' ? 'text-t-accent' : 'text-t-secondary-dark'}`}
           >
             <span>work</span>
             <FontAwesomeIcon width={12} height={12} icon={faArrowUpRightFromSquare} />
           </Link>
 
-          <Link 
-            to='/blog' 
+          <Link
+            to='/blog'
             className={`flex flex-center space-x-2 cursor-pointer hover:text-t-accent ${location.pathname === '/blog' ? 'text-t-accent' : 'text-t-secondary-dark'}`}
           >
             <span>blog</span>
             <FontAwesomeIcon width={12} height={12} icon={faArrowUpRightFromSquare} />
           </Link>
         </div>
+        <div className='flex flex-end text-t-secondary-dark space-x-6 pt-4'>
+          <a
+            href="https://drive.google.com/uc?export=download&id=1P86OijjHrYLnChetS9xVUkdbVk6nmRr4"
+            className='flex pt-4 space-x-2 text-t-secondary-dark cursor-pointer hover:text-t-accent md:pt-0'
+            download
+          >
+            <span>Download Resume</span>
+            <FontAwesomeIcon width={12} height={12} icon={faDownload} />
+          </a>
+        </div>
 
-        <a
-          href="https://drive.google.com/uc?export=download&id=1f8N8vZWh0t00GjVI34yyd-Eyrkt_rMKA"
-          className='flex  pt-4 space-x-2 text-t-secondary-dark cursor-pointer hover:text-t-accent'
-          download
-        >
-          <span>Download Resume</span>
-          <FontAwesomeIcon width={12} height={12} icon={faDownload} />
-        </a>
       </div>
     </>
   );
